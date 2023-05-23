62755143cda50e1cdf8be978ed253d188566f108
Evan Parsons <124839124+SquibbThompson@users.noreply.github.com>
2023-05-23 15:42:35 -0400

    Read Me
    Usage, dependencies, conditions, methodology

---
+++ Readme.py
@@ -0,0 +1,55 @@
+Auto Stack Programs
+
+This repository contains three programs that generate and animate matrices based on specified initial conditions. The programs are designed to create autonomic series with reflective stacking and dynamic updates. The matrices represent XOR and XNOR gates and undergo vertical and horizontal stacking. Each iteration of the animation updates the matrix according to certain rules.
+Programs
+Auto Stack 1.0
+
+Auto Stack 1.0 is a program that generates and displays a matrix based on specified initial conditions. The matrix represents an XOR or XNOR gate and undergoes vertical and horizontal stacking.
+Usage
+
+    Modify the initial conditions in the code, such as the XOR and XNOR gates.
+    Run the program.
+    Enter the gate choice (0 for XNOR, 1 for XOR).
+    Enter the number of vertical stacks.
+    Enter the number of horizontal stacks.
+    The program will generate and display the matrix based on the given inputs.
+
+Auto Stack 1.1
+
+Auto Stack 1.1 is a program that generates and animates a matrix based on specified initial conditions. The matrix represents an XOR or XNOR gate and undergoes vertical and horizontal stacking. Each iteration of the animation updates the matrix according to certain rules.
+Usage
+
+    Modify the initial conditions in the code, such as the XOR and XNOR gates.
+    Run the program.
+    Enter the gate choice (0 for XNOR, 1 for XOR).
+    Enter the number of vertical stacks.
+    Enter the number of horizontal stacks.
+    Enter the number of iterations.
+    The program will generate and animate the matrix based on the given inputs.
+
+Auto Stack 1.2
+
+Auto Stack 1.2 is a program that generates and animates a matrix based on specified initial conditions. The matrix represents an XOR or XNOR gate and undergoes vertical and horizontal stacking. Each iteration of the animation updates the matrix according to certain rules.
+Usage
+
+    Modify the initial conditions in the code, such as the XOR and XNOR gates.
+    Run the program.
+    Enter '1' for XOR or '0' for XNOR.
+    Enter the number of vertical stacks.
+    Enter the number of horizontal stacks.
+    Enter the number of horizontal iterations.
+    Enter the number of vertical iterations.
+    Enter the number of time steps.
+    The program will generate and animate the matrix based on the given inputs.
+
+Dependencies
+
+The following dependencies are required to run the programs:
+
+    numpy
+    matplotlib
+
+Make sure these libraries are installed before running the programs.
+License
+
+The programs are licensed under the MIT License. Feel free to modify and use the code according to the license terms.
